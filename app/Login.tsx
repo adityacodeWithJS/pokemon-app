e33eaeaf3538865b9e8a7e84dbd5c55c99b89629
Aditya Kumar2 <aditya_kumar2@epam.com>
2024-11-14 13:38:13 +0530

    added role wise test

--- app/Login.tsx
+++ app/Login.tsx
@@ -5,9 +5,17 @@ const [btnData, setBtnData]=useState<string>('')
 const handleBtn =()=>{
     setBtnData('Update Button')
 }
+const handleBtn2=()=>{
+    console.log("logs")
+}
 return (<div>
     <p>Login</p>
+    <div role="loginForm">
+
+    </div>
+    <label htmlFor="userEmailId">userName</label>
     <input type="text"
+    
      name="email" id="userEmailId"
        placeholder="Enter email"
        value={formData}
@@ -15,9 +23,15 @@ return (<div>
        />
 
        <button
+    
        onClick={handleBtn}
        >
        Update Button 
+       </button>
+       <button
+
+       >
+       Click
        </button>
 
 </div>)
--- app/__test__/Login.test.tsx
+++ app/__test__/Login.test.tsx
@@ -6,7 +6,7 @@ import { printName } from "../utils/printName";
 describe('Login Form', () => {
     test('input', () => {
        const loginComponent= render(<Login />)
-       expect(loginComponent).toMatchSnapshot()
+      // expect(loginComponent).toMatchSnapshot()
         const loginText = screen.getByText(/login/i)
         const inputEmail = screen.getByRole("textbox");
         const checkPlaceholder = screen.getByPlaceholderText("Enter email")
@@ -15,6 +15,7 @@ describe('Login Form', () => {
         expect(checkPlaceholder).toBeInTheDocument();
         expect(inputEmail).toHaveAttribute('name', 'email')
         expect(inputEmail).toHaveAttribute('id', 'userEmailId')
+    
         //expect(inputEmail).toHaveAttribute('value','aditya@gmail.com')
     })
 
@@ -28,11 +29,27 @@ describe('Login Form', () => {
 
     test('click event',()=>{
         render(<Login />)
-        const btn= screen.getByRole("button");
+        const btn2= screen.getByRole("button",{name:"Click"});
+        const btn= screen.getByRole("button",{name :'Update Button'});
+        expect(btn2).toBeInTheDocument()
         fireEvent.click(btn)
+        // fireEvent.click(btn2)
         expect(screen.getByText("Update Button")).toBeInTheDocument()
     })
     test('Functional Test',()=>{
      expect(printName()).toMatch('Aditya')
+    })
+
+    test('customRole',()=>{
+        render(<Login />)
+        const dv1= screen.getByRole('loginForm')
+        expect(dv1).toBeInTheDocument()
+    })
+    
+    test('inputLabel',()=>{
+        render(<Login/>)
+        const input1 = screen.getByRole('textbox',{name:"userName"})
+         expect(input1).toBeInTheDocument()
+    
     })
 })
